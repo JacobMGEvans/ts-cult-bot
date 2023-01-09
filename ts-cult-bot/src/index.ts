7191a78c474122a6e6b38d4e88d1280640ab13be
JacobMGEvans <jacobmgevans@gmail.com>
2023-01-08 21:16:44 -0600

    Middleware and Discord Interactions stuff

--- ts-cult-bot/src/index.ts
+++ ts-cult-bot/src/index.ts
@@ -1,10 +1,33 @@
 import { Hono } from "hono";
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKeyMiddleware,
+} from "discord-interactions";
 
-// never type to show that we don't have any Worker bindings yet
-const app = new Hono<{ Bindings: never }>();
+type Env = {
+  DISCORD_PUBLIC_KEY: string;
+};
+const app = new Hono<{ Bindings: Env }>();
 
-app.get("/", (c) => {
-  return c.text(`Hello World. ${JSON.stringify(c, null, 2)})}`);
+app.use("*", async (c, next) => {
+  verifyKeyMiddleware(c.env.DISCORD_PUBLIC_KEY);
+  await next();
+});
+
+app.get("/", async (c) => {
+  const message = await c.req.json();
+  // console.log(JSON.stringify(message, null, 2));
+  // if (message.type === InteractionType.PING) {
+  //   // The `PING` message is used during the initial webhook handshake, and is
+  //   // required to configure the webhook in the developer portal.
+  //   console.log("Handling Ping request");
+  //   return c.json({
+  //     type: JSON.stringify(InteractionResponseType.PONG),
+  //   });
+  // }
+
+  return c.json(message);
 });
 
 export default app;
