d753120a90af178ce1ef8d005dc95b3d6ac0b986
JacobMGEvans <jacobmgevans@gmail.com>
2023-01-08 22:51:32 -0600

    Ping

--- ts-cult-bot/src/index.ts
+++ ts-cult-bot/src/index.ts
@@ -7,6 +7,7 @@ import {
 
 type Env = {
   DISCORD_PUBLIC_KEY: string;
+  DISCORD_APPLICATION_ID: string;
 };
 const app = new Hono<{ Bindings: Env }>();
 
@@ -15,19 +16,26 @@ app.use("*", async (c, next) => {
   await next();
 });
 
-app.get("/", async (c) => {
-  const message = await c.req.json();
-  // console.log(JSON.stringify(message, null, 2));
-  // if (message.type === InteractionType.PING) {
-  //   // The `PING` message is used during the initial webhook handshake, and is
-  //   // required to configure the webhook in the developer portal.
-  //   console.log("Handling Ping request");
-  //   return c.json({
-  //     type: JSON.stringify(InteractionResponseType.PONG),
-  //   });
-  // }
+/**
+ * A simple :wave: hello page to verify the worker is working.
+ */
+app.get("/", ({ env }) => {
+  return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
+});
 
-  return c.json(message);
+app.post("/", async (c) => {
+  const message = await c.req.json();
+  if (message.type === InteractionType.PING) {
+    console.log(JSON.stringify(message, null, 2));
+    // The `PING` message is used during the initial webhook handshake, and is
+    // required to configure the webhook in the developer portal.
+    console.log("Handling Ping request");
+    return c.json({
+      type: JSON.stringify(InteractionResponseType.PONG),
+    });
+  }
+
+  return c.text(JSON.stringify(message, null, 2));
 });
 
 export default app;
