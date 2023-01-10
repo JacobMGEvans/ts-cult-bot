999bcd52dc14b405c8c0444c05c26323ce87b4f4
JacobMGEvans <jacobmgevans@gmail.com>
2023-01-10 00:01:32 -0600

    Modal started

--- bot/src/commands/create-job-posting.ts
+++ bot/src/commands/create-job-posting.ts
@@ -16,7 +16,8 @@ export const CreateJobPosting: Command = {
   name: "create-job-posting",
   description: "Request posting a job for Mod approval",
   type: ApplicationCommandType.ChatInput,
-  run: async (client: Client, interaction: CommandInteraction) => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  run: async (_client: Client, interaction: CommandInteraction) => {
     // if (interaction.commandName === 'create-job-posting') {
 
     // Create the modal
@@ -27,20 +28,15 @@ export const CreateJobPosting: Command = {
     const jobDescription = new TextInputBuilder()
       .setCustomId("jobDescription")
       .setLabel("Describe the job for the posting.")
-      // Paragraph means multiple lines of text.
       .setStyle(TextInputStyle.Paragraph);
 
-    // An action row only holds one text input,
-    // so you need one action row per text input.
     const firstTextActionRow =
       new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
         jobDescription
       );
 
-    // Add inputs to the modal
     modal.addComponents(firstTextActionRow);
 
-    // Show the modal to the user
     await interaction.showModal(modal);
 
     // if ((interaction as any).customId === 'myModal') {
