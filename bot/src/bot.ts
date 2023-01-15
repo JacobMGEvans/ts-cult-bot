f52c99390f692ad36fdbb6504c8861891cd4faa1
royanger <roy@royanger.com>
2023-01-14 23:35:32 -0500

    Updated prisma and config files

--- bot/src/bot.ts
+++ bot/src/bot.ts
@@ -3,21 +3,30 @@ import * as dotenv from "dotenv";
 import ready from "./listeners/ready";
 import interactionCreate from "./listeners/interactionCreate";
 import { PrismaClient } from "@prisma/client";
-dotenv.config();
+
+// grab the discord token from the .env.discord file
+// will this work in prod?
+dotenv.config({ path: ".env" });
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+// reset to general env vars for other stuff
+// also, will this work in prod?
+dotenv.config({ path: "../.env" });
+console.log("discord token: ", process.env.DATABASE_URL);
 
 export const prisma = new PrismaClient();
 
 console.log("Starting TS-Cult-Bot");
 
 // load environment variables -- see .env.sample for required envs
-const token = process.env.DISCORD_TOKEN;
+// const token = process.env.DISCORD_TOKEN;
 
 // test for missing environment variables
 const missingEnvVars: string[] = [];
 const checkEnvVar = (envVar: string | undefined, name: string) => {
   if (!envVar) missingEnvVars.push(name);
 };
-checkEnvVar(token, "DISCORD_TOKEN");
+checkEnvVar(DISCORD_TOKEN, "DISCORD_TOKEN");
 
 // error out if the environment variables are missing
 if (missingEnvVars.length > 0) {
@@ -33,4 +42,4 @@ const client = new Client({
 ready(client);
 interactionCreate(client);
 
-void client.login(token);
+void client.login(DISCORD_TOKEN);
