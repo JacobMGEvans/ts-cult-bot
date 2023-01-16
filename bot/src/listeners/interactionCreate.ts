2875afb633a6a40b4856a6093d0caa495dbf3880
JacobMGEvans <jacobmgevans@gmail.com>
2023-01-16 14:46:01 -0600

    Getting the Events from the Button clicks in Mod channel

--- bot/src/listeners/interactionCreate.ts
+++ bot/src/listeners/interactionCreate.ts
@@ -1,11 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { Commands } from "../commands";
-import type { CommandInteraction, Client, Interaction } from "discord.js";
+import type {
+  CommandInteraction,
+  Client,
+  Interaction,
+  ButtonInteraction,
+} from "discord.js";
 
 export default (client: Client): void => {
+  client.on("debug", (info) => {
+    console.dir(info, { depth: Infinity });
+  });
   client.on("interactionCreate", async (interaction: Interaction) => {
+    const modChannel = await client.channels.fetch(
+      process.env.JOB_POSTS_MODERATION_CHANNEL_ID ?? ""
+    );
+    // Slash Commands and User Context Menu Handlers
     if (interaction.isCommand() || interaction.isUserContextMenuCommand()) {
       await handleSlashCommand(client, interaction);
+    }
+    // Buttons in Mod Channel Handler
+    if (interaction.isButton() && interaction.channel === modChannel) {
+      await handleButtonsInModChannel(client, interaction);
     }
   });
 };
@@ -25,3 +41,27 @@ const handleSlashCommand = async (
 
   slashCommand.run(client, interaction);
 };
+
+const handleButtonsInModChannel = async (
+  client: Client,
+  interaction: ButtonInteraction
+): Promise<void> => {
+  console.dir(interaction, { depth: Infinity });
+  await Promise.resolve();
+  // const collector = message.createMessageComponentCollector({
+  //   componentType: ComponentType.Button,
+  //   time: 15000,
+  // });
+};
+
+// collector.on("collect", (i) => {
+//   if (i.user.id === interaction.user.id) {
+//     i.reply(`${i.user.id} clicked on the ${i.customId} button.`);
+//   } else {
+//     i.reply({ content: `These buttons aren't for you!`, ephemeral: true });
+//   }
+// });
+
+// collector.on("end", (collected) => {
+//   console.log(`Collected ${collected.size} interactions.`);
+// });
