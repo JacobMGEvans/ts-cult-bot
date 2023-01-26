6615515820cf5d19e9e0633e949a743e6bdf7e45
royanger <roy@royanger.com>
2023-01-26 17:52:27 -0500

    Redirect to dashboard on login

--- dashboard/src/pages/api/auth/[...nextauth].ts
+++ dashboard/src/pages/api/auth/[...nextauth].ts
@@ -9,6 +9,13 @@ import { prisma } from '../../../server/db';
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      // allow relative URLs
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      // disallow redirects to other origins
+      if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
     session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
@@ -23,15 +30,6 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here
-     *
-     * Most other providers require a bit more work than the Discord provider.
-     * For example, the GitHub provider requires you to add the
-     * `refresh_token_expires_in` field to the Account model. Refer to the
-     * NextAuth.js docs for the provider you want to use. Example:
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
